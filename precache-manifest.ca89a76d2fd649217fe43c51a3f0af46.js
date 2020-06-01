363ecfa07b015ec2e023ec843f30c1ba9000e190
Mai Anh Đào <maianhdao307@gmail.com>
2020-06-01 11:21:20 +0700

    Updates

---
+++ precache-manifest.ca89a76d2fd649217fe43c51a3f0af46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1ab62b817267974f6c34f7fe253ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "487dc18f9f6e8a264fc2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "029f1973d121c4012cb5",
+    "url": "/static/js/2.e9455646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9455646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487dc18f9f6e8a264fc2",
+    "url": "/static/js/main.e5084b5e.chunk.js"
+  },
+  {
+    "revision": "32762759838a60329c5c",
+    "url": "/static/js/runtime-main.8ac6ae30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
