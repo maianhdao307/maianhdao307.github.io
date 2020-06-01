72ea207eba7881f4d32b48252a11bf2e60e6b74e
Mai Anh Đào <maianhdao307@gmail.com>
2020-06-01 11:16:57 +0700

    Updates

---
+++ precache-manifest.75bcc804a0cea212d2c91127a176e83c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18334e88b3d5aa209af8ba49b93e96fe",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "62cc53d56763ed4fc859",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "029f1973d121c4012cb5",
+    "url": "/react/static/js/2.e9455646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.e9455646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62cc53d56763ed4fc859",
+    "url": "/react/static/js/main.b0d6170f.chunk.js"
+  },
+  {
+    "revision": "041fd42446d88ce6c994",
+    "url": "/react/static/js/runtime-main.23bf05de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
